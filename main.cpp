ec8eeac5c7dc7c9109b1a73bd48b8b9c0e548cb3
magyarakoos <ninjaoffical08@gmail.com>
2024-11-13 18:46:44 +0100

    Add PWM and LED example code

--- main.cpp
+++ main.cpp
@@ -1,19 +1,31 @@
+#include "hardware/pwm.h"
 #include "pico/cyw43_arch.h"
 #include "pico/stdlib.h"
+
+#include <algorithm>
 #include <iostream>
 
-const uint LED_PIN = CYW43_WL_GPIO_LED_PIN;
+#include "onboard_led.h"
+#include "pin.h"
+#include "pwm.h"
+
 const uint LED_DELAY_MS = 100;
+const uint BUZZER_PIN = 4;
 
 int main() {
-    cyw43_arch_init();
     stdio_init_all();
-    for (int i = 0 ;; i++) {
-        std::cout << "Hello, world! T = " << i << "\n";
 
-        cyw43_arch_gpio_put(LED_PIN, 1);
+    OnboardLed led;
+
+    PWM pwm = {BUZZER_PIN};
+
+    pwm.freq(15000);
+    pwm.duty(0.9);
+
+    while (true) {
+        led.toggle();
         sleep_ms(LED_DELAY_MS);
-        cyw43_arch_gpio_put(LED_PIN, 0);
+        led.toggle();
         sleep_ms(LED_DELAY_MS);
     }
 }
