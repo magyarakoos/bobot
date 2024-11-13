50b6d40b2433555fa5a894d6fa4c5c77411355f9
magyarakoos <ninjaoffical08@gmail.com>
2024-11-13 18:42:57 +0100

    Add PWM and LED example code

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "pico/cyw43_arch.h"
 #include "pico/stdlib.h"
+#include <iostream>
 
 const uint LED_PIN = CYW43_WL_GPIO_LED_PIN;
 const uint LED_DELAY_MS = 100;
@@ -7,7 +8,9 @@ const uint LED_DELAY_MS = 100;
 int main() {
     cyw43_arch_init();
     stdio_init_all();
-    while (true) {
+    for (int i = 0 ;; i++) {
+        std::cout << "Hello, world! T = " << i << "\n";
+
         cyw43_arch_gpio_put(LED_PIN, 1);
         sleep_ms(LED_DELAY_MS);
         cyw43_arch_gpio_put(LED_PIN, 0);
