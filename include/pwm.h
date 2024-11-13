18145b3ba8eeb88cb27202581f82fa28efd84c1f
magyarakoos <ninjaoffical08@gmail.com>
2024-11-13 18:48:34 +0100

    PWM driver

---
+++ include/pwm.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "pico/stdlib.h"
+#include "hardware/pwm.h"
+
+#include "pin.h"
+
+// abstraction for the hardware PWM's
+// it's useful because we can configure it with frequency 
+// and duty cycle, instead of wrap and level
+//
+// by default, the PWM is enabled
+class PWM {
+    uint pin;
+    uint frequency;
+    float duty_cycle;
+    uint slice_num;
+    uint channel_num;
+
+    // based on the current frequency and duty cycle,
+    // it calculates their corresponding wrap and level values
+    // and uses them to set the PWM
+    void calculate_values();
+
+public:
+    PWM(uint _pin);
+
+    void enable();
+    void disable();
+
+    // sets the number of PWM cycles per second
+    // currently, the valid range is only >=1907
+    void freq(uint _frequency);
+
+    // Value between 0 and 1, representing the percentage 
+    // of time the PWM signal is high within a single cycle
+    void duty(float _duty_cycle);
+};
+
+void setup_pwm(uint pin, uint frequency, float duty_cycle);
---
+++ src/pwm.cpp
@@ -0,0 +1,87 @@
+#include "pwm.h"
+#include <algorithm>
+#include <iostream>
+
+const uint CLOCK_SPEED = 125000000;
+
+void PWM::calculate_values() {
+
+    // calculate wrap and level
+    uint wrap = (CLOCK_SPEED / (float)frequency) - 1;
+    uint level = duty_cycle * (wrap + 1);
+
+    // configure the PWM slice for the desired frequency
+    // (wrap value)
+    pwm_set_wrap(slice_num, wrap);
+
+    // set the duty cycle (level value)
+    pwm_set_chan_level(slice_num, channel_num, level);
+}
+
+PWM::PWM(uint _pin)
+    : pin(_pin), frequency(0), duty_cycle(0),
+      slice_num(pwm_gpio_to_slice_num(_pin)),
+      channel_num(pwm_gpio_to_channel(_pin)) {
+    enable();
+}
+
+void PWM::enable() {
+    gpio_set_function(pin, GPIO_FUNC_PWM);
+    pwm_set_enabled(slice_num, true);
+}
+
+void PWM::disable() {
+    gpio_deinit(pin);
+}
+
+void PWM::freq(uint _frequency) {
+    frequency = _frequency;
+
+    // if we want a lower frequency, we'd need to increase
+    // the clock divider which introduces a ton of
+    // complexity
+    // TODO: solve this problem despite its complexity
+    frequency = std::clamp(frequency, 1907u, CLOCK_SPEED);
+
+    calculate_values();
+}
+
+void PWM::duty(float _duty_cycle) {
+    duty_cycle = _duty_cycle;
+
+    // duty cycle should be between 0 and 1
+    duty_cycle = std::clamp(duty_cycle, 0.0f, 1.0f);
+
+    calculate_values();
+}
+
+void setup_pwm(uint pin, uint frequency, float duty_cycle) {
+
+    gpio_set_function(pin, GPIO_FUNC_PWM);
+
+    // get the corresponding pwm slice
+    uint slice_num = pwm_gpio_to_slice_num(pin);
+
+    // if we want a lower frequency, we'd need to increase
+    // the clock divider which introduces a ton of
+    // complexity I might add it in the future though
+    frequency = std::clamp(frequency, 1907u, CLOCK_SPEED);
+
+    // duty_cycle should be in the range [0, 1]
+    duty_cycle = std::clamp(duty_cycle, 0.0f, 1.0f);
+
+    // calculate wrap and level
+    uint wrap = (CLOCK_SPEED / (float)frequency) - 1;
+    uint level = duty_cycle * (wrap + 1);
+
+    // configure the PWM slice for the desired frequency
+    // (wrap value)
+    pwm_set_wrap(slice_num, wrap);
+
+    // set the duty cycle (level value)
+    pwm_set_chan_level(slice_num, pwm_gpio_to_channel(pin),
+                       level);
+
+    // enable the PWM slice
+    pwm_set_enabled(slice_num, true);
+}
