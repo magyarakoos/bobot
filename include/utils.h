57b802747952a32fc084d325bbb8c51de56dc4a5
magyarakoos <magyar.kende.22f@gmail.com>
2025-07-04 21:44:30 +0200

    Create utils to remove dependency on algorithm and cmath

---
+++ include/utils.h
@@ -0,0 +1,10 @@
+#pragma once
+
+template <typename T>
+T clamp(const T& value, const T& min, const T& max);
+
+template <typename T>
+T abs(const T& x);
+
+template <typename T>
+T max(const T& x, const T& y);
