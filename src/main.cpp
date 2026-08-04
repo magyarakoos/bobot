e714a2209c747e26ac804dc4845d92c2512b1dea
magyarakoos <magyar.kende.22f@gmail.com>
2026-08-04 17:35:37 +0200

    Write a new example

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,11 @@
-#include <pico/stdlib.h>
-#include <string>
 #include "bobot.h"
 
 int main() {
     Bobot bobot;
-
-    TcpError x = bobot.setup_tcp();
-
-    if (x != TcpError::OK) {
-        while (true) {
-            printf("TCP setup failed\n");
-            sleep_ms(1000);
-        }
-    }
-
     while (true) {
-        bobot.tcp.send((std::to_string(bobot.ultra.dist()) + " cm").c_str());
-        printf("%f\n", bobot.ultra.dist());
+        bobot.led.on();
+        sleep_ms(500);
+        bobot.led.off();
         sleep_ms(500);
     }
 }
